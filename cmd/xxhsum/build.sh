8f0be54a8d5ccf68817143d8c996147ec5cbd795
OneOfOne <oneofone@gmail.com>
2019-11-05 12:42:16 -0600

    add -trimpath to build script

--- cmd/xxhsum/build.sh
+++ cmd/xxhsum/build.sh
@@ -2,5 +2,5 @@
 
 VERSION=$1
 
-env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o xxhsum64-${VERSION}.exe || exit 1
-env GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -ldflags "-s -w" -o xxhsum32-${VERSION}.exe || exit 1
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o xxhsum64-${VERSION}.exe || exit 1
+env GOOS=windows GOARCH=386 CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o xxhsum32-${VERSION}.exe || exit 1
